1f3811074291366626753c9922e0e28ea9338aae
Artur Mancha <joker@nadaradical.com>
2022-04-12 23:40:00 +0100

    Translation

--- commands/playMusic.js
+++ commands/playMusic.js
@@ -3,26 +3,26 @@ let connectionDispatcher;
 module.exports = (message, prefix) => {
     const voiceChannel = message.member.voice.channel;
 
-    if(message.content.startsWith(`${prefix} play`)) {
+    if(message.content.startsWith(`${prefix} ligar`)) {
         if(!voiceChannel) {
             return message.channel.send("Precisas de estar num canal de voz!");
         }
         const permissions = voiceChannel.permissionsFor(message.client.user);
         if(!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-            return message.channel.send("I don't have permissions to join and speak in that voice channel!");
+            return message.channel.send("Não tenho permissão para entrar e falar nesse canal de voz!");
         }
         
         voiceChannel.join().then(connection => {
             connectionDispatcher = connection.play("https://stream.ruc.pt/high.m3u");
             return message.channel.send("Rádio ligada.");
         });
-    } else if(message.content.startsWith(`${prefix} stop`)) {
+    } else if(message.content.startsWith(`${prefix} desligar`)) {
         if(connectionDispatcher) {
             connectionDispatcher.end();
             voiceChannel.leave();
             return message.channel.send("Rádio desligada.");
         }
-    } else if(message.content.startsWith(`${prefix} pause`)) {
+    } else if(message.content.startsWith(`${prefix} pausar`)) {
         if(connectionDispatcher) {
             if(connectionDispatcher) {
                 connectionDispatcher.end();
